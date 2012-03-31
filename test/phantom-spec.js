c5428886949e97db7c1ae58d2ae5067191d82570
TANIGUCHI Takaki <takaki@asis.media-as.org>
2012-03-31 20:34:12 +0900

    Imported Upstream version 1.5.0

--- test/phantom-spec.js
+++ test/phantom-spec.js
@@ -52,12 +52,12 @@ describe("phantom global object", function() {
         expect(phantom.version.major).toEqual(1);
     });
 
-    it("should return 4 as the minor version", function() {
-        expect(phantom.version.minor).toEqual(4);
+    it("should return 5 as the minor version", function() {
+        expect(phantom.version.minor).toEqual(5);
     });
 
-    it("should return 1 as the patch version", function() {
-        expect(phantom.version.patch).toEqual(1);
+    it("should return 0 as the patch version", function() {
+        expect(phantom.version.patch).toEqual(0);
     });
 
     it("should have 'injectJs' function", function() {
